2c3545cdcbca5618fbae22a4f7320b8be44100f3
Caitlin O'Reilly <coreilly29@gmail.com>
2022-12-29 12:41:07 -0500

    created react app and integrated premade components, installed and set up apollo client, created thoughtlist component, set up full stack structure with client-side react app

---
+++ client/src/pages/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+// import useQuery Hook from Apollo Client = able to make requests to GraphQL server
+// we connected to & made avail to app using <ApolloProvider> component in App.js
+import { useQuery } from "@apollo/client";
+// import QUERY_THOUGHTS query
+import { QUERY_THOUGHTS } from "../utils/queries";
+
+// import ThoughtList component
+import ThoughtList from "../components/ThoughtList";
+
+const Home = () => {
+  // use UseQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
+  return (
+    <main>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
